cc65e1384f60634cb88cb1941a09c1095412f8cd
SamBhattarai1 <53832665+SamBhattarai1@users.noreply.github.com>
2020-03-03 16:10:09 -0600

    New Feature Added

--- AnotherFile.py
+++ AnotherFile.py
@@ -1,8 +1,17 @@
+import numpy as np
 def find_all_letter(string_val):
     ''' prints all characters one by one'''
 
     for i in string_val:
         print (i)
 
+
+def random_generator(shuff_list=['AMERICOL','AMERIEBO']):
+
+    return  np.random.choice(shuff_list)
+
     
-find_all_letter('AMERICOLLECT')
+find_all_letter(random_generator())
+
+
+
